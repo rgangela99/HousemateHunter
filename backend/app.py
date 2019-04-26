3f745e6cabfb27269ee970cf1b00c2431415c066
rgangela99 <rgangela99@gmail.com>
2019-04-26 18:55:09 -0400

    Add basic database model and api endpoints

---
+++ backend/app.py
@@ -0,0 +1,61 @@
+import json
+from enum import Enum
+from flask import Flask, request
+from collections import defaultdict
+from db import db, User, Preferences
+
+app = Flask(__name__)
+db_filename = 'housemate_hunter.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_filename
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/api/users/', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    data = [user.serialize() for user in users]
+    return json.dumps({"success": True, "data": data}), 200
+
+
+@app.route('/api/users/', methods=['POST'])
+def post_user():
+    body = json.loads(request.data.decode('utf-8'))
+    user = User(
+        uuid=body.get('uuid'),
+        first_name=body.get('first_name'),
+        last_name=body.get('last_name'),
+        grad_year=body.get('grad_year'),
+        age=body.get('age'),
+        gender=body.get('gender'),
+        cleanliness=body.get('cleanliness'),
+        bio=body.get('bio')
+    )
+    db.session.add(user)
+    db.session.commit()
+    res = {"success": True, "data": user.serialize()}
+    return json.dumps(res), 201
+
+
+@app.route('/api/user/<int:user_id>/', methods=['GET'])
+def get_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return json.dumps({"success": False, "error": "User not found"}), 404
+    data = user.serialize()
+    return json.dumps({"success": True, "data": data}), 200
+
+
+@app.route('/api/user/<int:user_id>/', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return json.dumps({"success": False, "error": "User not found"}), 404
+    db.session.delete(user)
+    db.session.commit()
+    return json.dumps({"success": True, "data": user.serialize()}), 200
---
+++ backend/db.py
@@ -0,0 +1,41 @@
+from flask_sqlalchemy import SQLAlchemy
+from app_enums import Gender, SleepTime
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    uuid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    netid = db.Column(db.String, nullable=False)
+    grad_year = db.Column(db.Integer, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
+    cleanliness = db.Column(db.Integer, nullable=False)
+    location = db.Column()
+    bio = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    phone = db.Column(db.String)
+
+    def __init__(self, **kwargs):
+        self.uuid = kwargs.get('uuid')
+        self.name = kwargs.get('name')
+        self.grad_year = kwargs.get('grad_year')
+        self.age = kwargs.get('age')
+        self.gender = kwargs.get('gender')
+        self.cleanliness = kwargs.get('cleanliness')
+        self.bio = kwargs.get('bio')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "netid": self.netid,
+            "grad_year": self.grad_year,
+            "age": self.age,
+            "gender": self.gender,
+            "cleanliness": self.cleanliness,
+            "bio": self.bio
+        }
