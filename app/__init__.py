d798d5c4ba4981163209c0df89a7c99ee321df8c
rgangela99 <rgangela99@gmail.com>
2019-05-04 18:28:29 -0400

    Put things in the right file

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
-from .db import db, User
-from collections import defaultdict
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from enum import Enum
-import os
 import json
+import os
+from enum import Enum
+from flask import Flask, request
+from collections import defaultdict
+from db import db, User, Location
+import geocoder
+
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
 
 app = Flask(__name__)
-app.config.from_object(os.environ["APP_SETTINGS"])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 db.init_app(app)
@@ -24,22 +27,43 @@ def root():
 @app.route('/api/users/', methods=['GET'])
 def get_users():
     users = User.query.all()
-    # data = [user.serialize() for user in users]
-    return json.dumps({"success": True, "data": []}), 200
+    data = [user.serialize() for user in users]
+    return json.dumps({"success": True, "data": data}), 200
 
 
 @app.route('/api/users/', methods=['POST'])
 def post_user():
     body = json.loads(request.data.decode('utf-8'))
+    city = body.get('city')     # make capitalization consistent
+    state = body.get('state')   # turn into abbreviation
+    address = body.get('address')
+    g = geocoder.google(
+        "{}, {}, {}".format(address, city, state))
+    if not g.ok:
+        return json.dumps({"success": False, "error": "Invalid location"}), 400
+    latitude, longitude = g.latlng
+    location = Location.query.filter_by(latitude=latitude, longitute=longitude)
+    if not location:
+        location = Location(
+            city=city,
+            state=state,
+            address=address,
+            latitude=latitude,
+            longitute=longitude
+        )
     user = User(
         uuid=body.get('uuid'),
-        first_name=body.get('first_name'),
-        last_name=body.get('last_name'),
+        name=body.get('name'),
+        netid=body.get('netid'),
         grad_year=body.get('grad_year'),
         age=body.get('age'),
         gender=body.get('gender'),
+        sleep_time=body.get('sleep_time'),
         cleanliness=body.get('cleanliness'),
-        bio=body.get('bio')
+        bio=body.get('bio'),
+        email=body.get('email'),
+        phone=body.get('phone'),
+        location=location.id
     )
     db.session.add(user)
     db.session.commit()
@@ -49,7 +73,7 @@ def post_user():
 
 @app.route('/api/user/<int:user_id>/', methods=['GET'])
 def get_user(user_id):
-    user = User.query.filter_by(id=user_id).first()
+    user = User.query.filter_by(uuid=user_id).first()
     if not user:
         return json.dumps({"success": False, "error": "User not found"}), 404
     data = user.serialize()
@@ -58,7 +82,7 @@ def get_user(user_id):
 
 @app.route('/api/user/<int:user_id>/', methods=['DELETE'])
 def delete_user(user_id):
-    user = User.query.filter_by(id=user_id).first()
+    user = User.query.filter_by(uuid=user_id).first()
     if not user:
         return json.dumps({"success": False, "error": "User not found"}), 404
     db.session.delete(user)
--- app/db.py
+++ app/db.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     uuid = db.Column(db.String, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     netid = db.Column(db.String, nullable=False)
     grad_year = db.Column(db.Integer, nullable=False)
     age = db.Column(db.Integer, nullable=False)
