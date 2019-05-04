799a2406ec09d5872a21aada6f00592d1828c48b
rgangela99 <rgangela99@gmail.com>
2019-05-04 12:30:38 -0400

    Change foreign key from User to Location

--- app/db.py
+++ app/db.py
@@ -21,8 +21,7 @@ class User(db.Model):
     bio = db.Column(db.String, nullable=False)
     email = db.Column(db.String)
     phone = db.Column(db.String)
-    longitude = db.Column(db.Float, db.ForeignKey('location.longitude'))
-    latitude = db.Column(db.Float, db.ForeignKey('location.latitude'))
+    location_id = db.Column(db.Float, db.ForeignKey('location.id'))
     location = db.relationship('Location')
 
     def __init__(self, **kwargs):
